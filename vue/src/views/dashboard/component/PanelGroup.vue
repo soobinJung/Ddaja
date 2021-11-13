ab0ea03552199005976a5452597d66e66481e499
soobinJung <66097044+soobinJung@users.noreply.github.com>
2021-11-13 18:49:54 +0900

    대시보드 - 코드 정리 / 분리

--- vue/src/views/dashboard/component/PanelGroup.vue
+++ vue/src/views/dashboard/component/PanelGroup.vue
@@ -1,129 +1,76 @@
 <template>
-  <el-row :gutter="40" class="panel-group">
-
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="routerChange('/admin-ddaja/user')">
-        <div class="card-panel-icon-wrapper icon-people">
-          <svg-icon icon-class="peoples" class-name="card-panel-icon" />
-        </div>
-        <div class="card-panel-description">
-          <div class="card-panel-text">
-            User
+  <div style="padding: 5% 5% 0 5%">
+    <el-row 
+      :gutter = "40" 
+      class   = "panel-group">
+      <el-col 
+        v-for = "item in licenseOption"
+        :key  = "item.id"
+        :xs   = "12" 
+        :sm   = "12" 
+        :lg   = "6" 
+        class = "card-panel-col">
+        <div 
+          class  = "card-panel" 
+          @click = "routerChange('/explore', item)">
+          <div 
+            v-if  = "item.type === 'WRITING'" 
+            class = "card-panel-icon-wrapper icon1">
+            <svg-icon 
+              icon-class = "education" 
+              class-name = "card-panel-icon" />
           </div>
-          <count-to :start-val="0" :end-val="user.count" :duration="2600" class="card-panel-num" />
-        </div>
-      </div>
-    </el-col>
-
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="routerChange('/admin-ddaja/license')">
-        <div class="card-panel-icon-wrapper icon-message">
-          <svg-icon icon-class="tree" class-name="card-panel-icon" />
-        </div>
-        <div class="card-panel-description">
-          <div class="card-panel-text">
-            License
+          <div 
+            v-else 
+            class = "card-panel-icon-wrapper icon2">
+            <svg-icon 
+              icon-class = "education" 
+              class-name = "card-panel-icon" />
           </div>
-          <count-to :start-val="0" :end-val="license.count" :duration="3000" class="card-panel-num" />
-        </div>
-      </div>
-    </el-col>
-
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="routerChange('/admin-ddaja/notice')">
-        <div class="card-panel-icon-wrapper icon-money">
-          <svg-icon icon-class="edit" class-name="card-panel-icon" />
-        </div>
-        <div class="card-panel-description">
-          <div class="card-panel-text">
-            Notice
+          <div class = "card-panel-description">
+            <div 
+              class = "card-panel-text" 
+              style = "float:right; ">
+              <span style="float:right; font-weight : bold"> {{ item.name }} </span> 
+              <br/><br/>
+              <span 
+                style = "font-size:13px; float:right;"> 
+                {{ item.type === 'WRITING' ? "필기" : item.type === "PERFORM" ? "실기" : "" }}<br>
+              </span>
+            </div>
           </div>
-          <count-to :start-val="0" :end-val="notice.count" :duration="3200" class="card-panel-num" />
         </div>
-      </div>
-    </el-col>
-
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="routerChange('/admin-ddaja/question')">
-        <div class="card-panel-icon-wrapper icon-shopping">
-          <svg-icon icon-class="example" class-name="card-panel-icon" />
-        </div>
-        <div class="card-panel-description">
-          <div class="card-panel-text">
-            Question
-          </div>
-          <count-to :start-val="0" :end-val="question.count" :duration="3600" class="card-panel-num" />
-        </div>
-      </div>
-    </el-col>
-
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="routerChange('/admin-ddaja/word')">
-        <div class="card-panel-icon-wrapper icon-shopping">
-          <svg-icon icon-class="example" class-name="card-panel-icon" />
-        </div>
-        <div class="card-panel-description">
-          <div class="card-panel-text">
-            Word
-          </div>
-          <count-to :start-val="0" :end-val="word.count" :duration="3600" class="card-panel-num" />
-        </div>
-      </div>
-    </el-col>
-
-  </el-row>
+      </el-col> 
+    </el-row>
+    </div>
 </template>
-
 <script>
-import CountTo from 'vue-count-to'
 
 export default {
-  components: {
-    CountTo
-  },
+  props : {
+    licenseOption: {
+      type: Array
+      , default: true
+    }
+  }
 
-  data() {
+  , data() {
     return {
-      user: {
-        path: '',
-        count: 0
-      },
-      license: {
-        path: '',
-        count: 0
-      },
-      notice: {
-        path: '',
-        count: 0
-      },
-      question: {
-        path: '',
-        count: 0
-      },
-      word: {
-        path: '',
-        count: 0
+      param: {
+        name: ''
       }
     }
-  },
+  }
 
-  mounted() {
-    this.fetchData()
-  },
-
-  methods: {
-
-    fetchData() {
-      this.user.count = 234550
-      this.license.count = 7530
-      this.notice.count = 2550
-      this.question.count = 1520
-      this.word.count = 9640
-    },
-
-    routerChange(path) {
+  , methods: {
+    routerChange(path, licenseInfo) {
       this.$router.push(path)
-      this.$emit('handleSetLineChartData', 'purchases')
+      this.$session.set('licenseInfo', {
+        licenseID: licenseInfo.id,
+        licenseName: licenseInfo.name,
+        licenseCode: licenseInfo.code,
+        subject: licenseInfo.subjects
+      })
     }
   }
 }
@@ -152,40 +99,19 @@ export default {
       .card-panel-icon-wrapper {
         color: #fff;
       }
-
-      .icon-people {
+      .icon1 {
         background: #40c9c6;
       }
-
-      .icon-message {
-        background: #36a3f7;
-      }
-
-      .icon-money {
+      .icon2 {
         background: #f4516c;
-      }
-
-      .icon-shopping {
-        background: #34bfa3
-      }
+      } 
     }
-
-    .icon-people {
+    .icon1 {
       color: #40c9c6;
     }
-
-    .icon-message {
-      color: #36a3f7;
-    }
-
-    .icon-money {
+    .icon2 {
       color: #f4516c;
     }
-
-    .icon-shopping {
-      color: #34bfa3
-    }
-
     .card-panel-icon-wrapper {
       float: left;
       margin: 14px 0 0 14px;
@@ -193,25 +119,21 @@ export default {
       transition: all 0.38s ease-out;
       border-radius: 6px;
     }
-
     .card-panel-icon {
       float: left;
       font-size: 48px;
     }
-
     .card-panel-description {
       float: right;
       font-weight: bold;
       margin: 26px;
       margin-left: 0px;
-
       .card-panel-text {
         line-height: 18px;
         color: rgba(0, 0, 0, 0.45);
         font-size: 16px;
         margin-bottom: 12px;
       }
-
       .card-panel-num {
         font-size: 20px;
       }
@@ -223,13 +145,11 @@ export default {
   .card-panel-description {
     display: none;
   }
-
   .card-panel-icon-wrapper {
     float: none !important;
     width: 100%;
     height: 100%;
     margin: 0 !important;
-
     .svg-icon {
       display: block;
       margin: 14px auto !important;
