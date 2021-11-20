34f89fd0c5a75cb5c39af3bdbe8ea437bbadb6ac
soobinJung <66097044+soobinJung@users.noreply.github.com>
2021-11-20 17:37:47 +0900

    필요 없는 코드 삭제

--- BackEnd/ddaja/src/main/java/com/bng/ddaja/licenses/controller/LicensesController.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/licenses/controller/LicensesController.java
@@ -22,13 +22,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestBody;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @AllArgsConstructor
 @RequestMapping("licenses")
